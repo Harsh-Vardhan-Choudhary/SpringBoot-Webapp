bc532b8aa57b77eb9cf4e86c3ad4bcb565c57712
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-05-26 00:42:00 +0530

    Capturing Queryparams using ReqestParam & ModelMap

--- src/main/java/com/harsh/springboot/webapp/login/LoginController.java
+++ src/main/java/com/harsh/springboot/webapp/login/LoginController.java
@@ -1,13 +1,17 @@
 package com.harsh.springboot.webapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+        model.put("name", name);
+        System.out.println("Request param is : " + name);       //not recomm for prod. code
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
         <title>Login Page</title>
     </head>
     <body>
-        Welocome to the login page
+        Welocome to the login page, ${name}
     </body>
 </html>
