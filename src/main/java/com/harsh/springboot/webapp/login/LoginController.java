83103b1587a85d61ba5f749b3ab8bd8b1b1c0c9b
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-05-26 00:48:21 +0530

    Logging levels with springboot

--- src/main/java/com/harsh/springboot/webapp/login/LoginController.java
+++ src/main/java/com/harsh/springboot/webapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.harsh.springboot.webapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
     @RequestMapping("/login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Request param is : " + name);       //not recomm for prod. code
+        logger.debug("Request param is : " + name);
+        logger.info("print this at info level");
+        logger.warn("print this at warning level");
+        //System.out.println("Request param is : " + name);       //not recomm for prod. code
         return "login";
     }
 }
