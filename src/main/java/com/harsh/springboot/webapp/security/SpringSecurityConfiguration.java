18a02797da18fb1f373f5a00205aaa1bff1542bd
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-04 01:49:49 +0530

    Integrating H2 Database with Spring Boot

--- src/main/java/com/harsh/springboot/webapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/harsh/springboot/webapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.harsh.springboot.webapp.security;
 
 import java.util.function.Function;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration 
@@ -52,4 +56,17 @@ public class SpringSecurityConfiguration
     //here spring security is encoding the entered passeord with this BCryptPasswordEncoder algorithm 
     //& its not matching whats ever is in userDetail, so when we are configuring user details, 
     //also we would have to make use of that specific algo
+
+    //we are overriding SecurityFilterChain, we need to define entire chain again
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
 }
