a264ab591be7ef5a6f138d1d407846bacf25f375
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-04 00:10:04 +0530

    Adding Multiple Users

--- src/main/java/com/harsh/springboot/webapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/harsh/springboot/webapp/security/SpringSecurityConfiguration.java
@@ -22,17 +22,25 @@ public class SpringSecurityConfiguration
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager()
     {
+
+        UserDetails userDetails1 = createNewUser("kukki", "buddy");
+        UserDetails userDetails2 = createNewUser("harsh", "dummy");
+
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) 
+    {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 
         UserDetails userDetails = User.builder()
         .passwordEncoder(passwordEncoder)
-        .username("harsh")
-        .password("dummy")
+        .username(username)
+        .password(password)
         .roles("USER","ADMIN")
         .build();
-
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
